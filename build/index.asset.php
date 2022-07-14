06daefaa723199cc5a1877968762ccce647f2489
Kurt Rank <kurtrank@gmail.com>
2022-07-14 12:47:34 -0400

    chore: clean up build files

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => 'ef7829a00eca2f95c6e5');
